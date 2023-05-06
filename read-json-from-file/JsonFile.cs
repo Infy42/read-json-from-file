9b66763d86ccc9c83a279ca1bb6fd88e50133ac1
Infy42 <milovanmilic98@gmail.com>
2023-05-06 22:59:43 +0200

    read-json-from-file
    C#

---
+++ read-json-from-file/JsonFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace read_json_from_file
+{
+    class JsonFile
+    {
+        public string timestamp { get; set; }
+        public Employee[] employees { get; set; }
+
+        public void DisplayInfo()
+        {
+            foreach (Employee e in employees)
+            {
+                Console.WriteLine($"ID: {e.id}");
+                Console.WriteLine($"Name: {e.name}");
+                Console.WriteLine($"E-mail: {e.email}");
+                Console.WriteLine($"Birth date: {e.birth_date}");
+                Console.WriteLine($"Salary: {e.salary} usd/year");
+                Console.WriteLine($"Position: {e.position}");
+                Console.WriteLine($"Hired date: {e.hired_date}\n\n");
+            }
+        }
+
+    }
+
+    class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string birth_date { get; set; }
+        public int salary { get; set; }
+        public string position { get; set; }
+        public string hired_date { get; set; }
+    }
+}
---
+++ read-json-from-file/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+
+namespace read_json_from_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory + "\\employees.json";
+            string read = File.ReadAllText(path);
+            JsonFile jsonFile = JsonSerializer.Deserialize<JsonFile>(read);
+
+            jsonFile.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
